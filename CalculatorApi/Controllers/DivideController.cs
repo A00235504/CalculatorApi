bed3d538246220f79c2e0d2b0dc31b9ac5d062bd
aakashsadnani <A00235504@mycambrian.ca>
2021-10-26 18:04:26 -0400

    add ctrl for sub, mul, div & remove weather ctrl

---
+++ CalculatorApi/Controllers/DivideController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CalculatorLibrary;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CalculatorApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DivideController : Controller
+    {
+        CalculatorLibraryClass calculator = new CalculatorLibraryClass();
+
+        [HttpGet]
+        public decimal Get([FromQuery] decimal input1, [FromQuery] decimal input2)
+        {
+            return calculator.Divide(input1, input2);
+        }
+    }
+}
--- CalculatorLibrary/CalculatorLibraryClass.cs
+++ CalculatorLibrary/CalculatorLibraryClass.cs
@@ -7,5 +7,26 @@ namespace CalculatorLibrary
         {
             return (input1 + input2);
         }
+
+        public decimal Subtract(decimal input1, decimal input2)
+        {
+            return (input1 - input2);
+        }
+
+        public decimal Multiply(decimal input1, decimal input2)
+        {
+            return (input1 * input2);
+        }
+
+        public decimal Divide(decimal input1, decimal input2)
+        {
+            if (input1 == 0 || input2 == 0)
+            {
+                return -1;
+            }
+            else {
+                return (input1 / input2);
+            }
+        }
     }
 }
