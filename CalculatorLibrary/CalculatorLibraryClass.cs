bd7480f03daa7b2a78939cd27c513676ccb3e3fd
aakashsadnani <A00235504@mycambrian.ca>
2021-10-26 17:12:03 -0400

    Project Added and Addition api added
    Project added along with swagger and addition api controller and library

---
+++ CalculatorLibrary/CalculatorLibraryClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CalculatorLibrary
+{
+    public class CalculatorLibraryClass
+    {
+        public decimal Add(decimal input1, decimal input2)
+        {
+            return (input1 + input2);
+        }
+    }
+}
